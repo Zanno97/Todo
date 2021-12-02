280e98a059873aa4bca06e4b5fbf2d1f66b2ec26
Luca Zanolini <lucazanolini@Lucas-MacBook-Pro.local>
2021-12-02 12:57:35 +0100

    models && migration

---
+++ server/app/Models/Modify.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Traits\Modifiers;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Modify extends Model
+{
+    use HasFactory, Modifiers;
+}
---
+++ server/database/migrations/2021_12_02_113802_create_task_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task', function (Blueprint $table) {
+            $table->id();
+            $table->longText('description');
+            $table->boolean('ticked')->default(0);
+            $table->timestamp('tickedAt');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task');
+    }
+}
